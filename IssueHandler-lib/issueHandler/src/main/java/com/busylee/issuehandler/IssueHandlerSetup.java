6eeacb99ba6dd9b92a7f64ae348e77db84d19927
busylee999 <demonlee999@gmail.com>
2015-02-10 23:34:58 +0300

    fix annotation runtime policy

--- IssueHandler-lib/issueHandler/src/main/java/com/busylee/issuehandler/IssueHandlerSetup.java
+++ IssueHandler-lib/issueHandler/src/main/java/com/busylee/issuehandler/IssueHandlerSetup.java
@@ -1,8 +1,12 @@
 package com.busylee.issuehandler;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by busylee on 09.02.15.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface IssueHandlerSetup {
 
     enum IssueTracker {
